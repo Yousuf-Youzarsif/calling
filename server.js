82518876f6b0639046ce6c6f0c03a5986fbe1b84
Mohammed Yousuf <mohdyousuf00103@gmail.com>
2025-01-02 18:54:14 +0530

    Updated server port for Render deployment

--- server.js
+++ server.js
@@ -1,42 +1,3 @@
-// const express = require("express");
-// const http = require("http");
-// const { Server } = require("socket.io");
-
-// // Express App Setup
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// // Client Connection Handle
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
-
-//   // Offer Handle
-//   socket.on("offer", (data) => {
-//     socket.broadcast.emit("offer", data);
-//   });
-
-//   // Answer Handle
-//   socket.on("answer", (data) => {
-//     socket.broadcast.emit("answer", data);
-//   });
-
-//   // ICE Candidate Handle
-//   socket.on("candidate", (data) => {
-//     socket.broadcast.emit("candidate", data);
-//   });
-
-//   // Disconnect Handle
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected:", socket.id);
-//   });
-// });
-
-// // Server Start
-// const PORT = 3000;
-// server.listen(PORT, () => {
-//   console.log(`Signaling server running on http://localhost:${PORT}`);
-// });
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -72,7 +33,7 @@ io.on("connection", (socket) => {
 });
 
 // Server Start
-const PORT = process.env.PORT || 3000; // Use the dynamic port provided by Render
+const PORT = process.env.PORT || 3000; // Use Render's dynamic port or fallback to 3000 for local development
 server.listen(PORT, () => {
   console.log(`Signaling server running on http://localhost:${PORT}`);
 });
